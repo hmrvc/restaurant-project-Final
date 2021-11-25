8cbba23a314723b9221cff94600d8d32abc3b95e
hmrvc <hmrvc@yahoo.com.tw>
2021-11-25 19:55:54 +0800

    Set render data

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {shops: restaurantList.results})
 })
 
 app.listen(port, () => {

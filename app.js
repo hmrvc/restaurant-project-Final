ee0bf61edd606a90a31532a0b234df9216bec73c
hmrvc <hmrvc@yahoo.com.tw>
2021-11-25 19:38:32 +0800

    Import Handlebars

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+const { engine } = require('express-handlebars') 
 
 const port = 3000
 
+app.engine('handlebars', engine({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
 app.get('/', (req, res) => {
-  res.send('web built with express')
+  res.render('index')
 })
 
 app.listen(port, () => {

d27002aae453136fd4803c859833662471f34414
hmrvc <hmrvc@yahoo.com.tw>
2021-12-08 19:35:32 +0800

    Modify seeder with mongoose config

--- models/seeds/RestaurantSeeder.js
+++ models/seeds/RestaurantSeeder.js
@@ -1,17 +1,11 @@
 const mongoose = require('mongoose')
 const Restaurant = require('../restaurant.js')
 const restaurantList = require('../../restaurant.json').results
+const db = require('../../config/mongoose')
 
-mongoose.connect('mongodb://localhost:/restaurant-project')
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error')
-})
 
 db.once('open', () => {
   Restaurant.create(restaurantList)
   .then(console.log('restsurant loaded completed'))
-  .catch(error => console.log(error))
-  
+  .catch(error => console.log(error)) 
 })

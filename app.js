554b7b34e73fb4fafedec6dfd99c78913e8d066a
hmrvc <hmrvc@yahoo.com.tw>
2021-12-08 19:39:51 +0800

    Arrange app.js

--- app.js
+++ app.js
@@ -2,18 +2,12 @@ const express = require('express')
 const { engine } = require('express-handlebars') 
 const methodOverride = require('method-override')
 
-const Restaurant = require('./models/Restaurant')
-
 const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
 
-
-
 const port = 3000
-
-
 
 app.engine('handlebars', engine({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
@@ -22,71 +16,6 @@ app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
 app.use(routes)
-
-// // 顯示所有餐廳
-// app.get('/', (req, res) => {
-//   Restaurant.find()
-//   .lean()
-//   .then(shops => res.render('index', {shops}))
-//   .catch(error => console.log(error))
-// })
-
-
-// // 新增餐廳
-// app.get('/restaurants/new', (req, res) => {
-//   return res.render('new')
-// })
-
-// app.post('/restaurants/', (req, res) => {
-//   Restaurant.create(req.body)
-//     .then(() => res.redirect("/"))
-//     .catch(error => console.log(error))
-// })
-
-// // 顯示特定餐廳
-// app.get('/restaurants/:id', (req, res) => {
-//   const id = req.params.id
-//   return Restaurant.findById(id)
-//   .lean()
-//   .then(shop => res.render('show', {shop}))
-//   .catch(error => console.log(error))
-// })
-
-// // 搜尋餐廳
-// app.get('/search', (req, res) => {
-//   const keyword = req.query.keyword.toLowerCase().trim()
-//   Restaurant.find()
-//   .lean()
-//   .then(shops => {
-//     const filtershop = shops.filter(item => item.name.toLowerCase().includes(keyword) || item.category.includes(keyword))
-//     res.render('index', {shops: filtershop, keyword}) 
-//   })
-//   .catch(error => console.log(error))
-// })
-
-// // 修改餐廳
-// app.get('/restaurants/:id/edit', (req, res) => {
-//   const id = req.params.id
-//   return Restaurant.findById(id)
-//   .lean()
-//   .then((shop) => res.render('edit', {shop}))
-//   .catch(error => console.log(error))
-// })
-
-// app.put('/restaurants/:id', (req, res) => {
-//   const id = req.params.id
-//   Restaurant.findByIdAndUpdate(id, req.body)
-//   .then(() => res.redirect(`/restaurants/${id}`))
-//   .catch(error => console.log(error))
-// })
-
-// //刪除餐廳
-// app.delete('/restaurants/:id', (req, res) => {
-//   const id = req.params.id
-//   Restaurant.findByIdAndDelete(id)
-//   .then(() => res.redirect(`/`))
-//   .catch(error => console.log(error))
-// })
 
 app.listen(port, () => {
   console.log('connect')

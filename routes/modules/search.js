e3c4021017dddf681df312f5dcd1aa61fc14ed26
hmrvc <hmrvc@yahoo.com.tw>
2022-01-07 15:59:18 +0800

    Modify search function

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -2,10 +2,14 @@ const express = require('express')
 const router = express.Router()
 const Restaurant = require('../../models/Restaurant')
 
+
 // 搜尋餐廳
 router.get('/', (req, res) => {
+  const userId = req.user._id
   const keyword = req.query.keyword.toLowerCase().trim()
-  Restaurant.find()
+  
+  //加入Id條件只能搜尋自己的餐廳列表
+  Restaurant.find({ userId })
   .lean()
   .then(shops => {
     const filtershop = shops.filter(item => item.name.toLowerCase().includes(keyword) || item.category.includes(keyword))
